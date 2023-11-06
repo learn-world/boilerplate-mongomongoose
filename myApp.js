dc7c3c71d29a7ba3f9cdf89ded6477a832df2bfd
Shabin k <73272797+SHABIN-K@users.noreply.github.com>
2023-11-06 15:21:47 +0530

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const { Schema, model } = mongoose;
 
-
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
